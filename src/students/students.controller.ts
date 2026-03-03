16839911a1a342bee6f58228d3bd5eb2c7eefa44
Tala Mustafa <tallamoh@talas-macbook-air.home>
2026-03-03 08:10:07 +0300

    fix (students):add a type import for IDs and fix wiring in students module.

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -4,7 +4,7 @@ import { StudentsService } from './students.service';
 import { CreateStudentDTO } from './DTO/create.dto';
 import { Roles } from 'src/auth/roles.decorator';
 import { RoleId } from 'src/domain/user';
-import { ids } from 'src/domain/ids'; //try and fix this
+import type { UserId, StudentId } from 'src/domain/ids'; //try and fix this
 
 @Controller('students')
 export class StudentsController {
@@ -16,21 +16,21 @@ export class StudentsController {
   }
 
   @Get('myStudents')
-  async getStudents(userId: ids.UserId): Promise<Student[]> {
+  async getStudents(userId: UserId): Promise<Student[]> {
     return this.studentsService.getStudents(userId);
   }
 
   //add:
   //get students by id
   @Get('findOne')
-  async getById(studentId: ids.StudentId): Promise<Student> {
+  async getById(studentId: StudentId): Promise<Student> {
     return this.studentsService.getById(studentId);
   }
 
   @Roles([RoleId.Admin, RoleId.Teacher])
   @Patch('update')
   async updateStudent(
-    studentId: ids.StudentId,
+    studentId: StudentId,
     patch: Partial<Student>,
   ): Promise<Student> {
     return this.studentsService.updateStudent(studentId, patch);
--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { InMemoryStudentsRepo } from 'src/infrastructure/in-memory/in-memory-student.repo';
+import { InMemoryGradesRepo } from 'src/infrastructure/in-memory/in-memory-grade.repo';
+import { InMemoryUsersRepo } from 'src/infrastructure/in-memory/in-memory-user.repo';
 
 @Module({
   controllers: [StudentsController],
-  providers: [StudentsService, InMemoryStudentsRepo],
+  providers: [
+    StudentsService,
+    InMemoryStudentsRepo,
+    InMemoryGradesRepo,
+    InMemoryUsersRepo,
+  ],
 })
 export class StudentsModule {}
